a6acb69abbf0f1d40cd47ba3ca73a6c0a4b9f087
Bikram Shrestha <bikram.shrestha477@gmail.com>
2017-12-28 04:00:13 +0545

    'adminlt'

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Everest expedition, affordable well organized alpine mountain climbing expedition and base camp trek with experienced western guides, leaders, friendly sherpas, and good food to Everest, 7 summits, Nepal, Tibet, Himalaya, South America and Africa peaks.">
+    <meta name="author" content="">
+    <title>Summit Climb</title>
+    <!--fon-awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="/rajan/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- <link href="/rajan/css/custom.css" rel="stylesheet"> -->
+    <link href="{{ mix('css/frontend/frontend.css') }}" rel="stylesheet">
+</head>
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <div class="container">
+        <div id="main-navbar" class="navbar-fixed-top">
+            <div class="sc-top-statusbar">
+            </div>
+            <div class="sc-info">
+                <div class="sc-con">
+                    <ul>
+                        <li>Contact: <a href="#">0123456789</a></li>
+                        <li><a href="#">info@summitclimb.com</a></li>
+                    </ul>
+                </div>
+                <div class="sc-soc">
+                    <ul>
+                        <li>Get in Touch:</li>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-brand">
+            <a href="#">
+                <img src="/rajan/img/white-summit-logo.png" alt="summit-logo">
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Everest + More Climbs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Walking & Trekking peaks</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Charity</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link sc-join-btn" href="#">Join the team</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <!-- Slide One - Set the background image for this slide in the line below -->
+            <div class="carousel-item active" style="background-image: url('/rajan/img/slider1.jpg')">
+                <div class="sc-black-bg"></div>
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>Climbers are on the Famous Hillary Step, on the way
+                            to Summit of <strong>Everest from Nepal side.</strong></p>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Two - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('/rajan/img/slider2.jpg')">
+                <div class="sc-black-bg"></div>
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>Members are resting during our Lhotse and Everest climb at in the
+                            <strong>Western Cwm between camp 1 and camp 2.</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Three - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('/rajan/img/slider3.jpg')">
+                <div class="sc-black-bg"></div>
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p>Marin Minamiya, Japan youngest 8000 metre climber, became the worlds
+                            <strong>youngest woman to summit Mount Manaslu.</strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <i class="fa fa-angle-left" aria-hidden="true"></i>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</header>
+<!-- Page Content -->
+<section class="py-5">
+    <div class="container">
+        <h1>WHERE WOULD YOU LIKE TO CLIMB, WALK, HIKE, AND TREK?</h1>
+        <p>For more than 20 years we have been exploring mountains with friends from around the world, giving back to the regions, and encouraging those who appreciate nature.
+            We wish you a good visit. Thank you very much. Best regards from all of us at SummitClimb.com & SummitTrek.com</p>
+    </div>
+</section>
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; SummitClimb 2017</p>
+    </div>
+    <!-- /.container -->
+</footer>
+<!-- Bootstrap core JavaScript -->
+<script src="/rajan/vendor/jquery/jquery.min.js"></script>
+<script src="/rajan/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
